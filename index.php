9c633b2b9daf654b9d43c03f2a8a15b4c3d19c28
matason <hello@webgoodness.co.uk>
2013-05-03 14:43:00 +0100

    Moved require trulioo_client below the force HTTPS check so it's not included unnecessarily when redirecting.

--- index.php
+++ index.php
@@ -19,13 +19,13 @@
 // Defined in 'AppInfo.php'
 require_once('AppInfo.php');
 
-require_once('lib/trulio_client.php');
-
 // Enforce https on production
 if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
   header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
   exit();
 }
+
+require_once('lib/trulio_client.php');
 
 // This provides access to helper functions defined in 'utils.php'
 require_once('utils.php');
